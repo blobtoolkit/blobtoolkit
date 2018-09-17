e7d0740752e1692666dce4ca9f27734768ed07e0
Richard Challis <rjchallis@gmail.com>
2018-09-17 11:42:05 +0100

    add fileIO test

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # CI testing
 
 [![Build Status](https://travis-ci.org/rjchallis/test.svg?branch=master)](https://travis-ci.org/rjchallis/test)
-
 [![Coverage Status](https://coveralls.io/repos/github/rjchallis/test/badge.svg?branch=master)](https://coveralls.io/github/rjchallis/test?branch=master)
---
+++ lib/fileIO.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""functions to read, write and parse files."""
+
+import os
+
+def read_file(filename):
+    """
+    Reads a file.
+
+    >>> read_file('test/files/infile')
+    'testfile content\\n'
+    """
+    with open(filename, 'r') as fh:
+        return fh.read()
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
