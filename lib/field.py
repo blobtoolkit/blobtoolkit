029845d064fe7fe32f430940016a2d086896b4b8
Richard Challis <rjchallis@gmail.com>
2018-09-25 13:20:37 +0100

    add Variable methods

--- lib/field.py
+++ lib/field.py
@@ -5,11 +5,15 @@ Example Class module.
 Tests are included in docstrings and run using doctest.
 """
 
+from functools import reduce
+import numbers
+from operator import add
+
 
 class Field():
     """Parent class for specific field types."""
 
-    __slots__ = ['field_id', 'values', 'keys', 'type']
+    __slots__ = ['field_id', 'values', 'keys', 'type', '_subset']
 
     def __init__(self, field_id, **kwargs):
         """Init Field class."""
@@ -17,6 +21,7 @@ class Field():
         self.type = 'generic'
         self.values = []
         self.keys = []
+        self._subset = False
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -41,6 +46,72 @@ class Field():
                 values = set(values)
         indices = [i for i, x in enumerate(self.values) if x in values]
         return indices
+
+    def select_records(self, indices=False):
+        """Create a subset of values from a list of indices."""
+        values = []
+        if not isinstance(indices, list):
+            values = False
+        else:
+            values = self.get_values_by_indices(indices)
+        self.subset = values
+        return values
+
+    @property
+    def subset(self):
+        """Subset property."""
+        subset = []
+        if isinstance(self._subset, list):
+            subset = self._subset
+        else:
+            subset = self.values
+        return subset
+
+    @subset.setter
+    def subset(self, values=False):
+        """Set subset using indices."""
+        if isinstance(values, list):
+            self._subset = values
+        else:
+            self._subset = False
+
+
+class Variable(Field):
+    """Class for variable field type."""
+
+    def __init__(self, field_id, **kwargs):
+        """Init Field class."""
+        super().__init__(field_id, **kwargs)
+        self.type = 'variable'
+
+    def get_indices_in_range(self, min_max, invert=False):
+        """Get indices for all records in range."""
+        if not isinstance(min_max, list):
+            return -1
+        if not len(min_max) == 2:
+            return -2
+        if not all(isinstance(x, numbers.Real) for x in min_max):
+            return -3
+        indices = []
+        for index, value in enumerate(self.values):
+            if invert:
+                if value < min_max[0] or value > min_max[1]:
+                    indices.append(index)
+            else:
+                if min_max[0] <= value <= min_max[1]:
+                    indices.append(index)
+        return indices
+
+    def sum_values(self):
+        """Sum values in subset."""
+        try:
+            total = reduce(add, self.subset)
+        except TypeError:
+            total = 0
+        return total
+
+
+__all__ = ['Field', 'Variable']
 
 
 if __name__ == '__main__':
--- tests/unit_tests/test_field.py
+++ tests/unit_tests/test_field.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
 import pytest
-from lib import field
+from lib import Field, Variable
 
 FIELD_ID = 'test_field'
-FIELD_DATA = {'values': [1, 2, 3, 4, 5, 4, 3, 2]}
+FIELD_DATA = {'keys': ['first', 'second'], 'values': [1, 2, 3, 4, 5, 4, 3, 2]}
 VARIABLE_ID = 'test_variable'
 INTEGER_DATA = {'values': [1, 2, 3, 4, 5, 4, 3, 2]}
 FLOAT_DATA = {'values': [1.3, 4.2, 6.46733, 4.98, 2.45, 5.4, 3.643, 4.98]}
 STRING_DATA = {'values': ['A', 'B', 'third']}
-CATEGORY_DATA = {'keys': ['first', 'second'], 'values': [1, 3, 0, 0, 4, 1, 2, 2]}
+CATEGORY_DATA = {'keys': ['first', 'second', 'third', 'fourth', 'fifth'],
+                 'values': [1, 3, 0, 0, 4, 1, 2, 2]}
 ARRAY_DATA = {'keys': ['first', 'second'], 'values': [[1, 23, 67, 12], [0, 48, 96, 16]]}
 
 
 @pytest.fixture(scope='function', name='_my_field')
 def my_field():
-    return field.Field(FIELD_ID, **FIELD_DATA)
+    return Field(FIELD_ID, **FIELD_DATA)
 
 
-def test_init(_my_field):
+def test_field_init(_my_field):
     assert _my_field.field_id == FIELD_ID
     assert _my_field.values == FIELD_DATA['values']
+    assert _my_field.keys == FIELD_DATA['keys']
+    assert _my_field.type == 'generic'
 
 
 def test_get_values_by_index(_my_field):
@@ -33,15 +36,69 @@ def test_get_values_by_index(_my_field):
 
 
 def test_get_indices_by_values(_my_field):
-    assert _my_field.get_indices_by_values(4) == [3, 5]
-
-
-def test_get_indices_by_values_with_list(_my_field):
+    my_indices = _my_field.get_indices_by_values(4)
+    assert isinstance(my_indices, list)
+    assert my_indices == [3, 5]
     assert _my_field.get_indices_by_values([3]) == [2, 6]
-
-
-def test_get_indices_by_values_with_set(_my_field):
     assert _my_field.get_indices_by_values(set([2])) == [1, 7]
+
+
+def test_subset(_my_field):
+    assert _my_field.subset == FIELD_DATA['values']
+    _my_field.subset = [1, 3, 5]
+    assert _my_field.subset == [1, 3, 5]
+    _my_field.subset = []
+    assert _my_field.subset == []
+    _my_field.subset = False
+    assert _my_field.subset == FIELD_DATA['values']
+
+
+def test_select_records(_my_field, mocker):
+    mocked_get_values_by_indices = mocker.patch.object(Field, 'get_values_by_indices')
+    mocked_get_values_by_indices.return_value = [1, 3, 5]
+    values = _my_field.select_records([0, 2, 4])
+    mocked_get_values_by_indices.assert_called()
+    mocked_get_values_by_indices.assert_called_with([0, 2, 4])
+    assert values == [1, 3, 5]
+    assert _my_field.subset == [1, 3, 5]
+    mocked_get_values_by_indices.return_value = []
+    values = _my_field.select_records([])
+    assert values == []
+    values = _my_field.select_records(False)
+    assert values is False
+
+
+@pytest.fixture(scope='function', name='_my_variable')
+def my_variable():
+    return Variable(VARIABLE_ID, **INTEGER_DATA)
+
+
+def test_variable_init(_my_variable):
+    assert _my_variable.field_id == VARIABLE_ID
+    assert _my_variable.values == INTEGER_DATA['values']
+    assert _my_variable.type == 'variable'
+
+
+def test_get_indices_in_range(_my_variable):
+    assert _my_variable.get_indices_in_range(2, 3) == -1
+    assert _my_variable.get_indices_in_range([2]) == -2
+    assert _my_variable.get_indices_in_range(['1', '4']) == -3
+    my_indices = _my_variable.get_indices_in_range([2, 4])
+    assert isinstance(my_indices, list)
+    assert my_indices == [1, 2, 3, 5, 6, 7]
+    assert _my_variable.get_indices_in_range([2.3, 3.4]) == [2, 6]
+    assert _my_variable.get_indices_in_range([2.3, 2.5]) == []
+    assert _my_variable.get_indices_in_range([2, 4], True) == [0, 4]
+
+
+def test_sum_values(_my_variable):
+    assert _my_variable.sum_values() == 24
+    _my_variable.subset = [1, 3, 5]
+    assert _my_variable.sum_values() == 9
+    _my_variable.subset = False
+    assert _my_variable.sum_values() == 24
+    _my_variable.subset = []
+    assert _my_variable.sum_values() == 0
 
 
 # def test_load_yaml(mocker):
