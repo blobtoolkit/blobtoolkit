59bc629934023ff29305ee833a5590cebb529faa
Richard Challis <richard.challis@sanger.ac.uk>
2022-03-01 12:13:21 +0000

    "Bump version: 3.0.8 → 3.0.9"

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit",  # Required
-    version="3.0.8",
+    version="3.0.9",
     description="blobtoolkit",  # Optional
     long_description="blobtoolkit",  # Optional
     long_description_content_type="text/markdown",
--- src/blobtools/lib/version.py
+++ src/blobtools/lib/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 """blobtoolkit version."""
 
-__version__ = "blobtoolkit v3.0.8"
+__version__ = "blobtoolkit v3.0.9"
