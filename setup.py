bc3c5502c55d9860bb5da57123aa34e9a159879e
Richard Challis <richard.challis@sanger.ac.uk>
2022-05-02 13:28:35 +0100

    "Bump version: 3.1.3 → 3.1.4"

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit",  # Required
-    version="3.1.3",
+    version="3.1.4",
     description="blobtoolkit",  # Optional
     long_description="blobtoolkit",  # Optional
     long_description_content_type="text/markdown",
--- src/viewer/README.md
+++ src/viewer/README.md
@@ -1,4 +1,4 @@
-# BlobToolKit Viewer (v3.1.3)
+# BlobToolKit Viewer (v3.1.4)
 
 [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.1134794.svg)](https://doi.org/10.5281/zenodo.1134794)
 
