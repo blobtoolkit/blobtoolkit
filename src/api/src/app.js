8c5b004c8c6a0d49d87375e16bc89f74f4857289
Richard Challis <richard.challis@sanger.ac.uk>
2022-11-09 14:18:12 +0000

    update api packaging

--- src/api/src/app.js
+++ src/api/src/app.js
@@ -1,42 +1,24 @@
-const express = require("express");
-const compression = require("compression");
-const bodyParser = require("body-parser");
-const path = require("path");
 const config = require("./config/main");
-const logAccess = require("./functions/logger").logAccess;
-const logError = require("./functions/logger").logError;
-const resolve = require("path").resolve;
-const app = express();
+const app = require("./server");
 const searchRoutes = require("./routes/search_routes");
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, "public")));
-if (process.pkg) {
-  app.use(express.static(path.join(__dirname, "api-docs")));
-}
-app.locals.directory = resolve(config.filePath);
-if (config.cors) {
-  const cors = require("express-cors");
-  app.use(cors(config.cors));
-}
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  logAccess({ req });
-  next();
-});
-
-app.use((err, req, res, next) => {
-  let error = {
-    message: err.message,
-    errors: err.errors,
+if (config.https) {
+  const https = require("https");
+  const fs = require("fs");
+  var options = {
+    key: fs.readFileSync(config.keyFile),
+    cert: fs.readFileSync(config.certFile),
   };
-  res.status(err.status || 500).json(error);
-  logError({ ...error, req });
-});
-
-require("./routes")(app, {});
-
+  var secureServer = https
+    .createServer(options, app)
+    .listen(config.api_port, async function () {
+      console.log("blobtoolkit-api started on https port: " + config.api_port);
+      await searchRoutes.loadIndex();
+    });
+} else {
+  app.listen(config.api_port, async function () {
+    console.log("blobtoolkit-api started on http port: " + config.api_port);
+    await searchRoutes.loadIndex();
+  });
+}
 module.exports = app;
--- src/api/src/server.js
+++ src/api/src/server.js
@@ -1,23 +1,42 @@
+const express = require("express");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const path = require("path");
 const config = require("./config/main");
-const app = require("./app");
+const logAccess = require("./functions/logger").logAccess;
+const logError = require("./functions/logger").logError;
+const resolve = require("path").resolve;
+const app = express();
 const searchRoutes = require("./routes/search_routes");
 
-if (config.https) {
-  const https = require("https");
-  const fs = require("fs");
-  var options = {
-    key: fs.readFileSync(config.keyFile),
-    cert: fs.readFileSync(config.certFile),
-  };
-  var secureServer = https
-    .createServer(options, app)
-    .listen(config.api_port, async function () {
-      console.log("blobtoolkit-api started on https port: " + config.api_port);
-      await searchRoutes.loadIndex();
-    });
-} else {
-  app.listen(config.api_port, async function () {
-    console.log("blobtoolkit-api started on http port: " + config.api_port);
-    await searchRoutes.loadIndex();
-  });
+app.use(compression());
+app.use(express.static(path.join(__dirname, "public")));
+if (process.pkg) {
+  app.use(express.static(path.join(__dirname, "api-docs")));
 }
+app.locals.directory = resolve(config.filePath);
+if (config.cors) {
+  const cors = require("express-cors");
+  app.use(cors(config.cors));
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  logAccess({ req });
+  next();
+});
+
+app.use((err, req, res, next) => {
+  let error = {
+    message: err.message,
+    errors: err.errors,
+  };
+  res.status(err.status || 500).json(error);
+  logError({ ...error, req });
+});
+
+require("./routes")(app, {});
+
+module.exports = app;
