6afa41336bd1f17cb95fb1b88a6360aee27f59f5
Richard Challis <richard.challis@sanger.ac.uk>
2022-12-13 13:17:59 +0000

    support python dict argument

--- src/rust/cov-tools/src/lib.rs
+++ src/rust/cov-tools/src/lib.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::error::Error;
+use std::path::PathBuf;
 
 use cli::{Config, Options};
 
@@ -34,9 +36,77 @@ fn cov_filter(options: &Options) -> PyResult<usize> {
     Ok(read_names.len())
 }
 
+fn extract_to_option_pathbuf(
+    py: Python<'_>,
+    map: &HashMap<String, PyObject>,
+    key: &str,
+) -> Option<PathBuf> {
+    let hash_key = String::from(key);
+    let option: Option<PathBuf> = match map.get(&hash_key) {
+        Some(value) => Some(value.extract::<PathBuf>(py).unwrap()),
+        _ => None,
+    };
+    option
+}
+
+fn extract_to_default_string(
+    py: Python<'_>,
+    map: &HashMap<String, PyObject>,
+    key: &str,
+    default: &str,
+) -> String {
+    let hash_key = String::from(key);
+    let value = match map.get(&hash_key) {
+        Some(value) => value.extract::<String>(py).unwrap(),
+        _ => String::from(default),
+    };
+    value
+}
+
+fn extract_to_bool(py: Python<'_>, map: &HashMap<String, PyObject>, key: &str) -> bool {
+    let hash_key = String::from(key);
+    let value = match map.get(&hash_key) {
+        Some(value) => value.extract::<bool>(py).unwrap(),
+        _ => false,
+    };
+    value
+}
+
+fn convert_hashmap_to_options(py: Python<'_>, map: HashMap<String, PyObject>) -> Options {
+    let list = extract_to_option_pathbuf(py, &map, "list");
+    let bam = extract_to_option_pathbuf(py, &map, "bam");
+    let cram = extract_to_option_pathbuf(py, &map, "cram");
+    let fasta = extract_to_option_pathbuf(py, &map, "fasta");
+    let fastq1 = extract_to_option_pathbuf(py, &map, "fastq1");
+    let fastq2 = extract_to_option_pathbuf(py, &map, "fastq2");
+    let read_list = extract_to_option_pathbuf(py, &map, "read_list");
+    let suffix = extract_to_default_string(py, &map, "suffix", "filtered");
+    let fasta_out = extract_to_bool(py, &map, "fasta_out");
+    let fastq_out = extract_to_bool(py, &map, "fastq_out");
+    Options {
+        list,
+        bam,
+        cram,
+        fasta,
+        fastq1,
+        fastq2,
+        suffix,
+        fasta_out,
+        fastq_out,
+        read_list,
+    }
+}
+
+#[pyfunction]
+fn cov_filter_dict(py: Python<'_>, map: HashMap<String, PyObject>) -> PyResult<usize> {
+    let options = &convert_hashmap_to_options(py, map);
+    cov_filter(options)
+}
+
 #[pymodule]
 fn cov_tools(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(cov_filter, m)?)?;
+    m.add_function(wrap_pyfunction!(cov_filter_dict, m)?)?;
     m.add_class::<Options>()?;
     Ok(())
 }
