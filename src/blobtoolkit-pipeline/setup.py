7827bdd311d9a486cc285500ba33e7c1fa965f7a
Richard Challis <richard.challis@sanger.ac.uk>
2023-01-23 09:24:11 +0000

    Bump version: 3.5.5 → 4.0.0

--- src/blobtoolkit-pipeline/setup.py
+++ src/blobtoolkit-pipeline/setup.py
@@ -97,7 +97,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit-pipeline",  # Required
-    version="3.5.5",
+    version="4.0.0",
     description="blobtoolkit-pipeline",  # Optional
     long_description="blobtoolkit-pipeline",  # Optional
     long_description_content_type="text/markdown",
