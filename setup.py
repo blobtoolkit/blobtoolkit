c82c756447403c9e44bf80487ee2aadd6929f50c
Richard Challis <richard.challis@sanger.ac.uk>
2025-05-08 15:12:36 +0100

    Bump version: 4.4.5 → 4.4.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit",  # Required
-    version="4.4.5",
+    version="4.4.6",
     description="blobtoolkit",  # Optional
     long_description="blobtoolkit",  # Optional
     long_description_content_type="text/markdown",
@@ -141,9 +141,9 @@ setup(
             "pytest-mock>=3.1.1",
             "pytest>=6.0.0",
         ],
-        "full": ["blobtoolkit-host==4.4.1", "blobtoolkit-pipeline==4.4.3"],
+        "full": ["blobtoolkit-host==4.4.1", "blobtoolkit-pipeline==4.4.6"],
         "host": ["blobtoolkit-host==4.4.1"],
-        "pipeline": ["blobtoolkit-pipeline==4.4.3"],
+        "pipeline": ["blobtoolkit-pipeline==4.4.6"],
     },
     entry_points={
         "console_scripts": [
