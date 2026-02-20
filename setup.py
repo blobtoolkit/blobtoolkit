d5b30c3be250003485726efe16577533b55873f0
Richard Challis <richard.challis@sanger.ac.uk>
2026-02-20 13:26:48 +0000

    Bump version: 4.5.0 → 4.5.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit",  # Required
-    version="4.5.0",
+    version="4.5.1",
     description="blobtoolkit",  # Optional
     long_description="blobtoolkit",  # Optional
     long_description_content_type="text/markdown",
--- src/blobtoolkit-pipeline/src/README.md
+++ src/blobtoolkit-pipeline/src/README.md
@@ -1,4 +1,4 @@
-# BTK Pipeline v4.5.0
+# BTK Pipeline v4.5.1
 
 Splits original pipeline into sub-pipelines that can be run independently or using the `blobtoolkit.smk` meta pipeline.
 
