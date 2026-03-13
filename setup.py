e8540e66e6514238a80d760d8d9b265b6d4acbdf
Richard Challis <richard.challis@sanger.ac.uk>
2026-03-13 19:03:15 +0000

    Bump version: 4.5.1 → 4.5.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="blobtoolkit",  # Required
-    version="4.5.1",
+    version="4.5.2",
     description="blobtoolkit",  # Optional
     long_description="blobtoolkit",  # Optional
     long_description_content_type="text/markdown",
