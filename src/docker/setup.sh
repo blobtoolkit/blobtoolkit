6751dedb1019cddba07fa4c192610fb643dc9dcf
Richard Challis <richard.challis@sanger.ac.uk>
2026-03-14 08:06:47 +0000

    fall back to PyPI for host/pipeline when no local wheel built

--- src/docker/setup.sh
+++ src/docker/setup.sh
@@ -7,6 +7,13 @@ ls /tmp/*.whl | while read WHEEL; do
         $WHEEL
 done
 
+# Fall back to PyPI for host/pipeline if no local wheel was built for them
+$CONDA_DIR/envs/btk_env/bin/python -c "import importlib.metadata; importlib.metadata.version('blobtoolkit-host')" 2>/dev/null || \
+    $CONDA_DIR/envs/btk_env/bin/pip install --prefix $CONDA_DIR/envs/btk_env "blobtoolkit[host]"
+
+$CONDA_DIR/envs/btk_env/bin/python -c "import importlib.metadata; importlib.metadata.version('blobtoolkit-pipeline')" 2>/dev/null || \
+    $CONDA_DIR/envs/btk_env/bin/pip install --prefix $CONDA_DIR/envs/btk_env "blobtoolkit[pipeline]"
+
 if [ -s /blobtoolkit/blobtoolkit-api-linux ]; then
     mv /blobtoolkit/blobtoolkit-api-linux /blobtoolkit/blobtoolkit-api
     chmod 755 /blobtoolkit/blobtoolkit-api
